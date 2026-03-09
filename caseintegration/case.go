73ead3f7e85e878e515217e6e015d75bd675f7c4
Rob Maas <rob.maas@on2it.net>
2026-03-09 13:50:34 +0100

    GetCases

--- caseintegration/case.go
+++ caseintegration/case.go
@@ -22,7 +22,7 @@ type Case struct {
 	Note                string        `json:"note,omitempty"`             // Represents a note or update added to the case (used when creating a case)
 	Priority            int           `json:"priority"`                   // Case priority 1 to 4, where 1 is the highest priority
 	CaseType            string        `json:"case_type"`                  // The type of the case; `securityincident`, `incident`, `change`, `standardchange`, `inforequest`.
-	PrimaryContactEmail string        `json:"primary_contact_email"`      // The email address of the primary contact for the case, this should match a user in the system.
+	PrimaryContactEmail *string       `json:"primary_contact_email"`      // The email address of the primary contact for the case, this should match a user in the system. Can be null.
 	Attachment          string        `json:"attachment,omitempty"`       // A base64-encoded file attachment as a data URI. The MIME and encoding type (e.g., data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64) should be included in the data URI.
 	State               string        `json:"state,omitempty"`            // The current state of the case: `new`, `in_progress`, `awaiting_customer`, `on_hold`, `pending_engineering`, `request_close`, `request_close_by_customer`, `closed`
 	CreationDate        int64         `json:"creation_date,omitempty"`    // Unix timestamp when the case was created
@@ -172,6 +172,19 @@ func (ci *CaseIntegration) GetCaseByID(ctx context.Context, caseID string) (*Cas
 	}
 
 	return cases[0], nil
+}
+
+// GetCases gets all cases of the relation (based on the used API Token)
+// It returns all Case objects, handling pagination automatically.
+func (ci *CaseIntegration) GetCases(ctx context.Context) ([]*Case, error) {
+	if ctx == nil {
+		ctx = context.Background()
+	}
+
+	call := "integration/cases"
+	method := "GET"
+
+	return utils.GetAllPages[Case](ctx, ci.apiEndpoint+call, method, ci.apiClient)
 }
 
 // RequestCaseClose Requests the closure of an existing case
